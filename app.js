a80acbb3c22debf1aa7b56555ec985998c763da6
pineda.juan92 <pineda.juan92@gmail.com>
2022-01-05 21:23:41 -0500

    Get and post methods created to receive and show the input

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.urlencoded());
+
+app.get("/", (req, res) => {
+  res.send(
+    `<form method='POST' action='/'>
+        <input type='text'name="name" />
+        <button type='submit'>Enviar</button>
+    </form>`
+  );
+});
+
+app.post("/", (req, res) => {
+  res.send(`<h1>Hola ${req.body.name}!</h1>`);
+});
+
+app.listen(3000, () => console.log("Listening on port 3000"));
